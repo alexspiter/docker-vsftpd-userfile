1faf2c55bbe7d578e2ed91e2a2043cc9533a1f6e
alexspiter <51837109+alexspiter@users.noreply.github.com>
2021-11-16 21:42:44 +0300

    Update run-vsftpd.sh

--- run-vsftpd.sh
+++ run-vsftpd.sh
@@ -1,68 +1,47 @@
 #!/bin/bash
 
-# If no env var for FTP_USER has been specified, use 'admin':
-if [ "$FTP_USER" = "**String**" ]; then
-    export FTP_USER='admin'
-fi
+# по списку пользователей: создание каталогов виртуальных пользователей
+n=0
+bool=true
+while read user
+do
+  if [[ "$bool" == true && -n $user ]]; then
+    # объявление переменной среды
+    ((n++))
+    export FTP_USER$n=$user
+    # создание каталога пользователя
+    mkdir -p "/var/www/test_ftp.ru/local/$user"
+    bool=false
+  else
+    bool=true
+  fi
+done < "/etc/vsftpd/virtual_users.txt"
 
-# If no env var has been specified, generate a random password for FTP_USER:
-if [ "$FTP_PASS" = "**Random**" ]; then
-    export FTP_PASS=`cat /dev/urandom | tr -dc A-Z-a-z-0-9 | head -c${1:-16}`
-fi
+# присвоение владельца ftp:ftp всем файлам каталога пользователя
+chown -R ftp:ftp /var/www/test_ftp.ru/local
 
-# Do not log to STDOUT by default:
-if [ "$LOG_STDOUT" = "**Boolean**" ]; then
-    export LOG_STDOUT=''
-else
-    export LOG_STDOUT='Yes.'
-fi
-
-# Create home dir and update vsftpd user db:
-mkdir -p "/home/vsftpd/${FTP_USER}"
-chown -R ftp:ftp /home/vsftpd/
-
-echo -e "${FTP_USER}\n${FTP_PASS}" > /etc/vsftpd/virtual_users.txt
+# по списку пользователей: запись виртуальных пользователей в БД db4
 /usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
 
-# Set passive mode parameters:
-if [ "$PASV_ADDRESS" = "**IPv4**" ]; then
-    export PASV_ADDRESS=$(/sbin/ip route|awk '/default/ { print $3 }')
-fi
-
-echo "pasv_address=${PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_max_port=${PASV_MAX_PORT}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_min_port=${PASV_MIN_PORT}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_addr_resolve=${PASV_ADDR_RESOLVE}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_enable=${PASV_ENABLE}" >> /etc/vsftpd/vsftpd.conf
-echo "file_open_mode=${FILE_OPEN_MODE}" >> /etc/vsftpd/vsftpd.conf
-echo "local_umask=${LOCAL_UMASK}" >> /etc/vsftpd/vsftpd.conf
-echo "xferlog_std_format=${XFERLOG_STD_FORMAT}" >> /etc/vsftpd/vsftpd.conf
-echo "reverse_lookup_enable=${REVERSE_LOOKUP_ENABLE}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_promiscuous=${PASV_PROMISCUOUS}" >> /etc/vsftpd/vsftpd.conf
-echo "port_promiscuous=${PORT_PROMISCUOUS}" >> /etc/vsftpd/vsftpd.conf
-
 # Get log file path
-export LOG_FILE=`grep xferlog_file /etc/vsftpd/vsftpd.conf|cut -d= -f2`
+#export LOG_FILE=`grep xferlog_file /etc/vsftpd/vsftpd.conf|cut -d= -f2`
 
-# stdout server info:
-if [ ! $LOG_STDOUT ]; then
-cat << EOB
-	*************************************************
-	*                                               *
-	*    Docker image: fauria/vsftpd                *
-	*    https://github.com/fauria/docker-vsftpd    *
-	*                                               *
-	*************************************************
+# stdout логгирование в файл $LOG_FILE
+#/usr/bin/ln -sf /dev/stdout $LOG_FILE
 
-	SERVER SETTINGS
-	---------------
-	· FTP User: $FTP_USER
-	· FTP Password: $FTP_PASS
-	· Log file: $LOG_FILE
-	· Redirect vsftpd log to STDOUT: No.
-EOB
-else
-    /usr/bin/ln -sf /dev/stdout $LOG_FILE
+# Если сертификат отсутствует,то он генерируется
+if [ ! -e /etc/vsftpd/private/vsftpd.pem ]
+then
+    echo "Generating self-signed certificate"
+    mkdir -p /etc/vsftpd/private
+
+    openssl req -x509 -nodes -days 7300 \
+        -newkey rsa:2048 -keyout /etc/vsftpd/private/vsftpd.pem -out /etc/vsftpd/private/vsftpd.pem \
+        -subj "/C=RU/O=Test_ftp.ru/CN=test_ftp.ru"
+    openssl pkcs12 -export -out /etc/vsftpd/private/vsftpd.pkcs12 -in /etc/vsftpd/private/vsftpd.pem -passout pass:
+
+    chmod 755 /etc/vsftpd/private/vsftpd.pem
+    chmod 755 /etc/vsftpd/private/vsftpd.pkcs12
 fi
 
 # Run vsftpd:
